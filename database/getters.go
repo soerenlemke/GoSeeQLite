49ac4e83963f9ef131fb5a99628f782c8bbfd510
soerenlemke <soerenlemke@gmx.de>
2024-05-18 23:29:07 +0200

    Added: - chinook.db added as example database for working with in the main.go and examples later - Getting the column infos for tables working now. Example added for iterating over all tables of the given database

--- database/getters.go
+++ database/getters.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"path/filepath"
 )
 
@@ -20,6 +22,12 @@ func (g *Get) AllTableNames() ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			log.Println("Error closing rows:", err)
+		}
+	}(rows)
 
 	var tableNames []string
 	for rows.Next() {
@@ -32,4 +40,43 @@ func (g *Get) AllTableNames() ([]string, error) {
 	}
 
 	return tableNames, nil
+}
+
+type ColumnInfo struct {
+	Name string
+	Type string
+}
+
+type TableColumn struct {
+	Id           int
+	Name         string
+	Type         string
+	NotNull      int
+	DefaultValue sql.NullString
+	PrimaryKey   int
+}
+
+func (g *Get) TableColumns(t string) ([]ColumnInfo, error) {
+	queryString := fmt.Sprintf("PRAGMA table_info(%s);", t)
+	rows, err := g.DB.connection.Query(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var result []ColumnInfo
+	for rows.Next() {
+		var tc TableColumn
+		err := rows.Scan(&tc.Id, &tc.Name, &tc.Type, &tc.NotNull, &tc.DefaultValue, &tc.PrimaryKey)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, ColumnInfo{Name: tc.Name, Type: tc.Type})
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
--- database/getters_test.go
+++ database/getters_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 )
 
-
 func TestGet_AllTableNames(t *testing.T) {
 	type fields struct {
 		DB *Database
@@ -59,6 +58,39 @@ func TestGet_DatabaseName(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("DatabaseName() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGet_TableColumns(t *testing.T) {
+	type fields struct {
+		DB *Database
+	}
+	type args struct {
+		t string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &Get{
+				DB: tt.fields.DB,
+			}
+			got, err := g.TableColumns(tt.args.t)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TableColumns() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TableColumns() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
