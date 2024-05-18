184a5139f101669be7af5bc83198d57178533d7a
Vyshnav Vinod <vyshnav.vinod003@gmail.com>
2024-05-19 00:58:53 +0530

    reorder files

---
+++ database/getters_test.go
@@ -0,0 +1,65 @@
+package database
+
+import (
+	"reflect"
+	"testing"
+)
+
+
+func TestGet_AllTableNames(t *testing.T) {
+	type fields struct {
+		DB *Database
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &Get{
+				DB: tt.fields.DB,
+			}
+			got, err := g.AllTableNames()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AllTableNames() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("AllTableNames() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGet_DatabaseName(t *testing.T) {
+	type fields struct {
+		DB *Database
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &Get{
+				DB: tt.fields.DB,
+			}
+			got, err := g.DatabaseName()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DatabaseName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("DatabaseName() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
