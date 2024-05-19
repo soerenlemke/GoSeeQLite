f436b98a4001dfcc203af378beaf558322df4752
Vyshnav Vinod <vyshnav.vinod003@gmail.com>
2024-05-19 11:37:39 +0530

    Check for invalid/non-existing db file

--- database/setup.go
+++ database/setup.go
@@ -1,10 +1,16 @@
 package database
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func NewDatabase(pathToDb string) (Database, error) {
 	if pathToDb == "" {
 		return Database{}, fmt.Errorf("path to db can't be empty")
+	}
+	if _, err := os.Stat(pathToDb); os.IsNotExist(err){
+		return Database{}, fmt.Errorf("file does not exist")
 	}
 
 	d := Database{
@@ -16,6 +22,12 @@ func NewDatabase(pathToDb string) (Database, error) {
 	err := d.Connect()
 	if err != nil {
 		return Database{}, fmt.Errorf("can`t connect to database: %s", d.dsn)
+	}
+
+	err, ok := d.ConnectionStatus()
+	if !ok {
+		// The file is not a database
+		return Database{}, err
 	}
 
 	return d, err
