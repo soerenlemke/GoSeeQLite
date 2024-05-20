857e3bc3334c7bfb5b8f667b6a3421ae166b0eab
Vyshnav Vinod <154617184+vyshnav-vinod@users.noreply.github.com>
2024-05-20 14:54:31 +0530

    This should finish the testing for now (#18)
    * Add connection status test
    
    Remove test for close
    
    Seems unnecessary
    
    * Add get tablenames test
    
    * FInish tests

--- database/connection.go
+++ database/connection.go
@@ -8,6 +8,9 @@ import (
 )
 
 func (db *Database) Connect() error {
+	// Does not really check if the database file 
+	// is a valid database. This is done by ConnectionStatus()
+
 	var err error
 	db.connection, err = sql.Open("sqlite3", db.dsn)
 	if err != nil {
@@ -27,6 +30,8 @@ func (db *Database) Close() error {
 }
 
 func (db *Database) ConnectionStatus() (err error, ok bool) {
+	// Returns error if the file is not a valid database
+	
 	err = db.connection.Ping()
 	if err != nil {
 		return err, false
--- database/getters_test.go
+++ database/getters_test.go
@@ -1,13 +1,15 @@
 package database
 
 import (
+	"path/filepath"
 	"reflect"
 	"testing"
 )
 
 func TestGet_AllTableNames(t *testing.T) {
+	testSampleValidDb, _ := filepath.Abs(filepath.Join("testdata", "chinook.db"))
 	type fields struct {
-		DB *Database
+		dsn string
 	}
 	tests := []struct {
 		name    string
@@ -15,82 +17,53 @@ func TestGet_AllTableNames(t *testing.T) {
 		want    []string
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			"Valid database", fields{dsn: testSampleValidDb}, []string{"albums", "sqlite_sequence", "artists", "customers", "employees", "genres", "invoices", "invoice_items", "media_types", "playlists", "playlist_track", "tracks", "sqlite_stat1"}, false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			g := &Get{
-				DB: tt.fields.DB,
-			}
-			got, err := g.AllTableNames()
+			db, _ := NewDatabase(tt.fields.dsn)
+			got, err := db.Get.AllTableNames()
 			if (err != nil) != tt.wantErr {
 				t.Errorf("AllTableNames() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("AllTableNames() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestGet_DatabaseName(t *testing.T) {
-	type fields struct {
-		DB *Database
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		want    string
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			g := &Get{
-				DB: tt.fields.DB,
-			}
-			got, err := g.DatabaseName()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("DatabaseName() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("DatabaseName() got = %v, want %v", got, tt.want)
+				t.Fatalf("AllTableNames() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
 func TestGet_TableColumns(t *testing.T) {
-	type fields struct {
-		DB *Database
-	}
-	type args struct {
-		t string
-	}
 	tests := []struct {
 		name    string
-		fields  fields
-		args    args
-		want    []string
+		args    string
+		wantFk  []bool // Since each column also has a value that shows if it is Fk or not
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			"Valid table with no foreign key", "artists", []bool{false, false}, false,
+		},
+		{
+			"Valid table with foreign key", "albums", []bool{false, false, true}, false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			g := &Get{
-				DB: tt.fields.DB,
-			}
-			got, err := g.TableColumns(tt.args.t)
+			testSampleValidDb, _ := filepath.Abs(filepath.Join("testdata", "chinook.db"))
+			db, _ := NewDatabase(testSampleValidDb)
+			got, err := db.Get.TableColumns(tt.args)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("TableColumns() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TableColumns() got = %v, want %v", got, tt.want)
+			for i, col := range got {
+				if col.ForeignKey != tt.wantFk[i] {
+					t.Errorf("TableColumns() error : Table : %s Column : %s WantFk : %t GotFk : %t", tt.args, col.Name, tt.wantFk[i], col.ForeignKey)
+					return
+				}
 			}
 		})
 	}
